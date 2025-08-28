94d6cf245184d84c0360a1719a00a0351276fe8f
Ayushman-Choudhuri <ayushc205@gmail.com>
2025-08-29 00:17:45 +0200

    added include files

---
+++ Algorithms/AStar/include/astar/board.hpp
@@ -0,0 +1,38 @@
+#ifndef ASTAR_BOARD_HPP
+#define ASTAR_BOARD_HPP
+
+#include <vector>
+#include <string>
+#include "astar/types.hpp"
+
+namespace astar {
+
+class Board {
+
+public:
+    Board() = default;
+
+    bool LoadFromFile(const std::string& filepath);
+
+    void Print() const;
+
+    State GetState(int x, int y) const;
+    void SetState(int x, int y, State state);
+
+    bool IsValidCell(int x, int y) const;
+    bool IsEmpty(int x, int y) const; 
+
+    size_t GetRows() const; //can be optimized with inline optimization
+    size_t GetCols() const; //can be optimized with inline optimization
+
+    std::vector<std::vector<State>>& getGrid(); //can be optimized with inline optimization
+
+private: 
+    std::vector<std::vector<State>> grid_;
+
+    std::vector<State> ParseLine(const std::string& line);
+};
+
+}
+
+#endif
---
+++ Algorithms/AStar/include/astar/types.hpp
@@ -0,0 +1,39 @@
+#ifndef ASTAR_TYPE_HPP
+#define ASTAR_TYPE_HPP
+
+namespace astar {
+
+enum class State {
+    kEmpty,
+    kObstacle,
+    kClosed,
+    kPath,
+    kStart,
+    kGoal
+};
+
+
+struct Point {
+    int x;
+    int y;
+
+    Point(int x = 0, int y = 0): x(x) , y(y) {}
+
+    bool operator==(const Point& other) const {
+        return (x == other.x && y == other.y);
+    }
+};
+
+struct Node {
+    Point position;
+    int g;
+    int h;
+
+    Node(int x , int y, int g_cost , int h_cost)
+        : position(x, y), g(g_cost), h(h_cost) {}
+
+};
+
+}
+
+#endif
